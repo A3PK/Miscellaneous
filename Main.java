76bb929ae55c0ac71411ee36242aca76b55b093c
Janaki <janaki@Janakis-iMac.local>
2021-02-12 18:54:13 +0530

    deleted some garbage, moven Main Problem to 2018 CCO day 2 problem 3

--- Main.java
+++ Main.java
@@ -1,27 +1,26 @@
-import java.util.LinkedHashMap;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    public int[] arrayFlopper(int[] args) {
+        if (args.length == 1) {
+            return args;
+        }
+        int[] next = arrayFlopper(Arrays.copyOf(args, args.length - 1));
+        int[] result = new int[2 + next.length];
+        result[0] = args.length;
+        result[1] = args[args.length - 1];
+        System.arraycopy(next, 0, result, 2, next.length);
+        return result;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < num; i++) {
-            LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
-            char[] chars = sc.nextLine().toCharArray();
-            for (char c : chars) {
-                if (map.containsKey(c)) {
-                    map.put(c, map.get(c) + 1);
-                }
-                else {
-                    map.put(c, 1);
-                }
-            }
-            for (char c : map.keySet()) {
-                System.out.print(map.get(c) + " " + c + " ");
-            }
-            System.out.print("\n");
-        }
+        sc.nextLine();
+        int[] input = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] output = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt)
+                             .toArray();
+        int flops = 0;
     }
 }
 
