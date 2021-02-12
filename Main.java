f8b5468edf2f94293040cb9a76629730e7b96e7b
Janaki <janaki@Janakis-iMac.local>
2021-02-12 15:27:50 +0530

    2019 CCC Cold compress solution

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -7,8 +7,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int num = Integer.parseInt(sc.nextLine());
         for (int i = 0; i < num; i++) {
-            HashMap<Integer, Character> map = new HashMap<>();
-            
+            LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+            char[] chars = sc.nextLine().toCharArray();
+            for (char c : chars) {
+                if (map.containsKey(c)) {
+                    map.put(c, map.get(c) + 1);
+                }
+                else {
+                    map.put(c, 1);
+                }
+            }
+            for (char c : map.keySet()) {
+                System.out.print(map.get(c) + " " + c + " ");
+            }
+            System.out.print("\n");
         }
     }
 }
