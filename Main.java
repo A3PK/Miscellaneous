53914d6e35fc075e055f4654a734d7f8bc5bb97a
Janaki <janaki@Janakis-iMac.local>
2021-02-12 14:59:27 +0530

    Throwing everything together in my file into my Misc. git

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < num; i++) {
+            HashMap<Integer, Character> map = new HashMap<>();
+            
+        }
+    }
+}
+
