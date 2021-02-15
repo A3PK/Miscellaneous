7f614bd857671d7d346db7c53e1ccb49446b8bdb
Janaki <janaki@Janakis-iMac.local>
2021-02-15 16:18:08 +0530

    finished program to return distinctDates with bad recursion

--- Main.java
+++ Main.java
@@ -1,11 +1,29 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
+    public static boolean isDistinct(int n) {
+        char[] chars = Integer.toString(n).toCharArray();
+        HashSet<Character> set = new HashSet<>();
+        for (char c : chars) {
+            if (set.contains(c)) {
+                return false;
+            }
+            set.add(c);
+        }
+        return true;
+    }
+
+    public static int returnDistinctDate(int n) {
+        if (isDistinct(n + 1)) {
+            return n + 1;
+        }
+        return returnDistinctDate(n + 1);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int y = sc.nextInt();
-        int m = sc.nextInt();
-        int difference = m - y;
-        System.out.println(m + difference);
+        int date = sc.nextInt();
+        System.out.println(returnDistinctDate(date));
     }
 }
