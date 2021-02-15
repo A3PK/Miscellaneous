8afba1f83856c59b3a65d3fe57012d1ce1fe2694
Janaki <janaki@Janakis-iMac.local>
2021-02-15 15:30:01 +0530

    finished 2014 assigning partners

--- Main.java
+++ Main.java
@@ -1,59 +1,23 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    public static boolean isConsonant(String s) {
-        if (s.equals("a") || s.equals("e") || s.equals("i") || s.equals("o") || s.equals("u")) {
-            return false;
-        }
-        return true;
-    }
-
-    public static String returnClosestVowel(String s) {
-        char c = s.charAt(0);
-        int[] distances = {
-                (int) 'a' - (int) c, (int) 'e' - (int) c, (int) 'i' - (int) c, (int) 'o' - (int) c,
-                (int) 'u' - (int) c,
-                };
-        char[] chars = { 'a', 'e', 'i', 'o', 'u' };
-        int largest = 999999;
-        int largestIdx = 0;
-        for (int i = 0; i < distances.length; i++) {
-            distances[i] = Math.abs(distances[i]);
-            if (distances[i] < largest) {
-                largest = distances[i];
-                largestIdx = i;
-            }
-        }
-        return Character.toString(chars[largestIdx]);
-    }
-
-    public static String returnNextConsonant(String s) {
-        char c = s.charAt(0);
-        char next = (char) ((int) c + 1);
-        if (c == 'z') {
-            return Character.toString('z');
-        }
-        if (!isConsonant(Character.toString(next))) {
-            return returnNextConsonant(Character.toString(next));
-        }
-        return Character.toString(next);
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] letters = sc.nextLine().split("");
-        String[] answer = new String[letters.length];
-        for (int i = 0; i < letters.length; i++) {
-            if (isConsonant(letters[i])) {
-                answer[i] = letters[i] + returnClosestVowel(letters[i]) + returnNextConsonant(
-                        letters[i]);
-            }
-            else {
-                answer[i] = letters[i];
+        sc.nextLine();
+        String[] str = sc.nextLine().split(" ");
+        HashMap<String, String> map = new HashMap<>();
+        String[] str2 = sc.nextLine().split(" ");
+        for (int i = 0; i < str.length; i++) {
+            map.put(str[i], str2[i]);
+        }
+        boolean good = true;
+        for (String s : map.keySet()) {
+            if (!s.equals(map.get(map.get(s))) || map.get(s).equals(s)) {
+                good = false;
+                break;
             }
         }
-        for (String s : answer) {
-            System.out.print(s);
-        }
+        System.out.println(good ? "good" : "bad");
     }
 }
