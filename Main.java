e206cc0d2bcab825fe6453eb3854096f9e2b69c9
Janaki <arnav.kukkle@gmail.com>
2021-02-15 16:48:31 +0530

    checking

--- Main.java
+++ Main.java
@@ -1,28 +1,86 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the speed limit:");
-        int limit = sc.nextInt();
-        System.out.println("Enter the recorded speed of the car:");
-        int speed = sc.nextInt();
-        if (speed <= limit) {
-            System.out.println("Congratulations, you are within the speed limit!");
+    public Integer[] printDivisors(int n) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                if (n / i == i) {
+                    list.add(i);
+                }
+
+                else {
+                    list.add(i);
+                    list.add(n / i);
+                }
+            }
         }
-        else {
-            int fine;
-            if (speed - limit <= 20) {
-                fine = 100;
+        Integer[] arr = list.stream().toArray(Integer[]::new);
+        return arr;
+    }
+
+    public boolean findIfPathExists(BufferedReader br) throws IOException {
+        int rows = Integer.parseInt(br.readLine());
+        int cols = Integer.parseInt(br.readLine());
+        String[][] grid = new String[rows][cols];
+        boolean[][] visited = new boolean[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            grid[i] = br.readLine().split(" ");
+            for (int j = 0; j < cols; j++) {
+                visited[i][j] = false;
             }
-            else if (speed - limit <= 30) {
-                fine = 270;
-            }
-            else {
-                fine = 500;
-            }
-            System.out.println("You are speeding and your fine is $" + fine);
         }
+        int n = Integer.parseInt(grid[0][0]);
+        int currRow = 1;
+        int currCol = 1;
+        boolean done = true;
+        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> row = new LinkedList<>();
+        Queue<Integer> col = new LinkedList<>();
+        while (n != Integer.parseInt(grid[rows - 1][cols - 1]) || currRow != rows
+                || currCol != cols) {
+            Integer[] divisors = printDivisors(n);
+            for (int i : divisors) {
+                if (i <= rows && n / i <= cols) {
+                    if (!visited[i - 1][(n / i) - 1]) {
+                        q.add(Integer.parseInt(grid[i - 1][(n / i) - 1]));
+                        row.add(i);
+                        col.add(n / i);
+                        visited[i - 1][(n / i) - 1] = true;
+                    }
+                }
+                if (n / i <= rows && i <= cols) {
+                    if (!visited[(n / i) - 1][i - 1]) {
+                        q.add(Integer.parseInt(grid[(n / i) - 1][i - 1]));
+                        row.add(n / i);
+                        col.add(i);
+                        visited[(n / i) - 1][i - 1] = true;
+                    }
+                }
+            }
+            if (q.size() == 0) {
+                done = false;
+                break;
+            }
+            n = q.remove();
+            currRow = row.remove();
+            currCol = col.remove();
+        }
+        return done;
+    }
+
+    public static void main(String[] args) throws IOException {
+        InputStreamReader r = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(r);
+
+        Main m = new Main();
+        // System.out.println(Arrays.toString(m.printDivisors(sc.nextInt())));
+        System.out.println(m.findIfPathExists(br) ? "yes" : "no");
     }
 }
